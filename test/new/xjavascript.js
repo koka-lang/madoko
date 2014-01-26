a2f71ea6b65fb750c3ff8dc13198a9dfa6cc593a
Daan Leijen <daan@microsoft.com>
2014-01-26 10:25:10 -0800

    Initial syntax highlighting theme for LaTeX

--- test/new/xjavascript.js
+++ test/new/xjavascript.js
@@ -1,6 +1,8 @@
 module.exports = function(hljs) {
   var js  = hljs.getLanguage("javascript");
   var xjs = Object.create(js);
-  xjs.keywords = "alert " + xjs.keywords;
+  var kws = Object.create(xjs.keywords);
+  kws.special = "alert";
+  xjs.keywords = kws;
   return xjs;
 }
