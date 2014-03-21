8990d6a51c640d60eff986c77b734e7032f79a71
daan <none@none>
2014-03-21 16:56:53 -0700

    Modularize main so we can share code for the web based madoko

---
+++ src/storage-inline.js
@@ -0,0 +1,63 @@
+/*---------------------------------------------------------------------------
+  Copyright 2013 Microsoft Corporation.
+ 
+  This is free software; you can redistribute it and/or modify it under the
+  terms of the Apache License, Version 2.0. A copy of the License can be
+  found in the file "license.txt" at the root of this distribution.
+---------------------------------------------------------------------------*/
+
+// Provide client/server storage
+// This module provides file access operations. On the web client, these
+// operations only provide accesss to a global object of files which is
+// *not* persistent. This code is mainly to ensure we can share as much
+// code as possible between client and server.
+var onServer = ($std_core.getHost() !== "browser");
+
+var $readFileSync;
+var $writeFileSync;
+var $fexistsSync;
+var $relative;
+var $mkdirp;
+var $cwd;
+
+if (onServer) {
+  var fs = require("fs");
+  var path = require("path");
+  var xmkdirp = require('mkdirp');
+
+  $readFileSync = function(fname) { return fs.readFileSync(fname,{encoding:'utf8'}); };
+  $writeFileSync = function(fname,data) { return fs.writeFileSync(fname,data,{encoding:'utf8'}); };
+  $fexistsSync = function(fname) { return (fs.existsSync(fname) != 0);}
+  $relative = function(dir,p) { return path.relative(dir,p); }
+  $cwd = function() { return process.cwd(); }
+  $mkdirp = function(dir,mode) { return xmkdirp.sync(dir,mode); }
+}
+else {
+  var vfs = {};  // virtual file system
+
+  $readFileSync = function(fname) {
+    var data = vfs["$" + fname];
+    if (data === undefined) throw ("Could not read: " + fname);
+    return data;
+  }
+
+  $writeFileSync = function(fname,data) {
+    vfs["$" + fname] = data;
+  }
+
+  $fexistsSync = function(fname) {
+    return (vfs["$" + fname] !== undefined);
+  }
+
+  $relative = function(dir,p) {
+    return p; // TODO: implement this client side.
+  }
+
+  $cwd = function() {
+    return "."; // ??
+  }
+
+  $mkdirp = function(dir,mode) {
+    // do nothing
+  }
+}
