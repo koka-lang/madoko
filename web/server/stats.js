4dd62dbba699f68feee62f901750310b23e6475b
daan <none@none>
2014-08-07 12:35:33 -0700

    More work on statistics

--- web/server/stats.js
+++ web/server/stats.js
@@ -57,8 +57,26 @@ function sum(xs) {
 }
 
 function avg(xs) {
-	if (xs==null || xs.length <= 0) return 0;
-	return (sum(xs) / xs.length);
+	var total = 0;
+	var n = 0;
+	if (xs != null && xs.length > 0) {
+		for(var i = 0; i < xs.length; i++) {
+			total += xs[i];
+			if (xs[i] != 0) n++;
+		}
+	}
+	if (n==0) return 0;
+	return Math.round(total/n);
+}
+
+
+
+function max(xs) {
+	var m = 0;
+	for(var i = 0; i < xs.length; i++) {
+		if (xs[i] > m) m = xs[i];
+	}
+	return m;
 }
 
 function digestUsers(entries) {
@@ -68,32 +86,58 @@ function digestUsers(entries) {
 		var userEntries = users.getOrCreate(entry.user.id, []);
 		userEntries.push(entry);
 	});
+	users = users.map( function(uentries) {
+		var delta = 10*60*1000; // 10 minutes
+		var total = 60*1000;    // 1 minute
+		var prevTime = 0;
+		uentries.forEach( function(entry) {
+			var nextTime = date.dateFromISO(entry.date).getTime();
+			if (prevTime===0) {
+				prevTime = nextTime;
+			}
+			else if (prevTime + delta > nextTime) {
+				total += (nextTime - prevTime);
+				prevTime = nextTime;
+			}
+		});
+		return {
+			workTime: total,
+			reqCount: uentries.length,
+		}
+	});
 	return users.elems();
 }
 
 function digestDaily(entries) {
-	entries = entries.filter( function(entry) { return (entry.user != null); } );
+	entries = entries.filter( function(entry) { return (entry.user != null && entry.time != null); } );
 	console.log("total user entries: " + entries.length);
 	var daily = new Map();
 	entries.forEach( function(entry) {
 		var date = entry.date.replace(/T.*/,"");
 		var dateEntries = daily.getOrCreate(date,[]);
-		if (entry.files) {
-			entry.size = sum( entry.files.map( function(file) { return file.size; } ) );
-		}
-		else {
-			entry.size = 0;
+		if (!entry.size) {
+			if (entry.files) {
+				entry.size = sum( entry.files.map( function(file) { return file.size; } ) );
+			}
+			else {
+				entry.size = 0;
+			}
 		}
 		dateEntries.push(entry);
 	});
 	console.log(daily.length)
 	daily = daily.map( function(dentries) {		
 		var users = digestUsers(dentries);
+		var runEntries = dentries.filter( function(entry) { return entry.url === "/rest/run"; })
 		return { 
 			userCount: users.length,
 			reqCount: dentries.length,
-			avgTime: avg( dentries.map( function(entry) { return entry.time; }) ),
-			avgSize: avg( dentries.map( function(entry) { return entry.size; }) ),
+			avgWorkTime  : Math.ceil( avg( users.map( function(entry) { return entry.workTime; }) ) / (60*1000) ),
+			maxWorkTime  : Math.ceil( max( users.map( function(entry) { return entry.workTime; }) ) / (60*1000) ),
+			avgServerTime: avg( runEntries.map( function(entry) { return entry.time; }) ),
+			maxServerTime: max( runEntries.map( function(entry) { return entry.time; }) ),
+			avgServerSize: avg( runEntries.map( function(entry) { return entry.size; }) ),
+			maxServerSize: max( runEntries.map( function(entry) { return entry.size; }) ),
 			//entries: dentries.map( function(entry) { return entry.user.id; } ),
 		};
 	});
