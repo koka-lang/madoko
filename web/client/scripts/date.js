220362cada2b69b4141a661c8f979cb88c7964ef
daan <none@none>
2014-08-07 12:02:05 -0700

    Fixed bug in ISO date parsing with milliseconds

--- web/client/scripts/date.js
+++ web/client/scripts/date.js
@@ -15,13 +15,13 @@ define([],function() {
       return (isNaN(i) ? undefined : i);
     }
 
-    var rxISO = /^(\d\d\d\d)\-?(\d\d)\-?(\d\d)(?:T(\d\d):?(\d\d)(?:[:]?(\d\d))?(?:Z|([\+\-])(\d\d)(?:[:]?(\d\d))?)?)?$/i;
+    var rxISO = /^(\d\d\d\d)\-?(\d\d)\-?(\d\d)(?:T(\d\d):?(\d\d)(?:[:]?(\d\d)(?:[\.,](\d\d\d))?)?(?:Z|([\+\-])(\d\d)(?:[:]?(\d\d))?)?)?$/i;
     var cap = rxISO.exec( s.replace(/\s+/g, "") );
     if (!cap) return new Date(0);    
     var utc  = new Date( Date.UTC( parseNum(cap[1]), parseNum(cap[2])-1, parseNum(cap[3]),
-                                   parseNum(cap[4]), parseNum(cap[5]), parseNum(cap[6]) ) );
+                                   parseNum(cap[4]), parseNum(cap[5]), parseNum(cap[6]), parseNum(cap[7]) ) );
     if (!utc || isNaN(utc)) return new Date(0);
-    var tz = (cap[9]=="+" ? -1 : 0) * ((parseNum(cap[8])||0) * 60 + (parseNum(cap[9])||0));
+    var tz = (cap[8]=="+" ? -1 : 0) * ((parseNum(cap[9])||0) * 60 + (parseNum(cap[10])||0));
     if (tz !== 0) utc.setUTCMinutes( utc.getUTCMinutes + tz );
     return utc;
   }
