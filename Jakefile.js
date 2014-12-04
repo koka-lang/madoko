5d230d2d05fb8b23adc2de8201aa09efd58fe6ce
daan <none@none>
2014-12-04 14:23:20 -0800

    Copy LaTeX style files to subdirectory in web styles.

--- Jakefile.js
+++ Jakefile.js
@@ -129,6 +129,7 @@ task("webcopy", ["web"], function() {
   // copy sty files to local texmf tree
   var sty = new jake.FileList().include(path.join(styleDir,"*.sty"));
   copyFiles(styleDir,sty.toArray(),localTexDir);
+  copyFiles(styleDir,sty.toArray(),path.join(webclient,styleDir,"latex"))
 }); 
 
 
