4917a1ae132bd3c457f587c1d3c825e5eba1525e
daan <none@none>
2014-12-16 10:23:08 -0800

    Fix new onedrive download url

--- web/server.js
+++ web/server.js
@@ -1309,7 +1309,7 @@ app.get("/rest/remote/onedrive", function(req,res) {
   event( req, res, true, function() {
     var login = req.session.logins.onedrive;
     if (!login || !login.access_token) throw { httpCode: 401, message: "Must be logged in to request Onedrive content" };
-    if (!/https:\/\/[\w\-\.]+?\.livefilestore\.com\//.test(req.query.url)) {
+    if (!/https:\/\/[\w\-\.]+?\.(livefilestore|files\.1drv)\.com\//.test(req.query.url)) {
       throw { httpCode: 403, message: "Illegal onedrive url: " + req.query.url };
     }
     return requestGET( req.query.url, "binary" );
