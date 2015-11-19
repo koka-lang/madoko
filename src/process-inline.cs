37f82578d8aa8548a07cfe1ab76c19176a0e7874
daan <none@none>
2015-11-19 12:55:49 -0800

    Fixes to CSharp backend

--- src/process-inline.cs
+++ src/process-inline.cs
@@ -6,28 +6,28 @@
   found in the file "license.txt" at the root of this distribution.
 ---------------------------------------------------------------------------*/
 
-using System;
 using System.Diagnostics;
+using System.Threading;
 
 public static class ShellCommand
 {
   // cmd : string, callback : (int,string,string) -> <io|e> (), timeout : int = 0, cwd : string = "") : <io|e> () {
-  public static object system( string cmd, Fun3<int,string,string,object> callback, int timeout, string cwd ) {
-    var pinfo = new ProcessStartInfo();
+  public static object system( string cmd, Fun3<int,string,string,Unit> callback, int timeout, string cwd ) {
+    ProcessStartInfo pinfo = new ProcessStartInfo();
     pinfo.UseShellExecute = true;
-    if (cwd != null && cwd.length > 0) pinfo.WorkingDirectory = cwd;
+    if (cwd != null && cwd.Length > 0) pinfo.WorkingDirectory = cwd;
     pinfo.FileName = "cmd.exe";
     pinfo.Verb = "runas";
     pinfo.Arguments = "/c " + cmd;
     pinfo.WindowStyle = ProcessWindowStyle.Hidden;
     pinfo.RedirectStandardError = true;
     pinfo.RedirectStandardOutput = true;
-    var process = new Process();
+    Process process = new Process();
     process.StartInfo = pinfo;
     process.EnableRaisingEvents = true;
     process.Exited += new EventHandler( delegate(object sender, System.EventArgs args ) {
-      var stdout = process.StandardOutput.ReadToEnd();
-      var stderr = process.StandardError.ReadToEnd();
+      string stdout = process.StandardOutput.ReadToEnd();
+      string stderr = process.StandardError.ReadToEnd();
       callback.Apply( process.ExitCode, stdout, stderr );
       return;
     });
@@ -38,10 +38,11 @@ public static class ShellCommand
                       try {
                         process.Kill(); 
                       } 
-                      catch() {}
+                      catch(Exception) {}
                     }
                  }),
                  null, timeout, Timeout.Infinite );
     }
+    return Unit.unit;
   }
 }
