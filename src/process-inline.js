8cf848e369e40f4d12a2427799a81081797e720f
Daan <none@none>
2015-12-25 18:48:56 -0800

    Check for correct dvisvgm version; don't run dvisvgm concurrently

--- src/process-inline.js
+++ src/process-inline.js
@@ -5,28 +5,3 @@
   terms of the Apache License, Version 2.0. A copy of the License can be
   found in the file "license.txt" at the root of this distribution.
 ---------------------------------------------------------------------------*/
-
-/*
-function execProcess( cmd, options, cb ) {
-  var args = cmd.split(/\s+/).map(function(arg) { return unquote(arg); } );
-  var child = child_process.spawn( args[0], args.slice(1), options );
-  var stderr = "";
-  var stdout = "";
-  child.stdin.write("\n");
-  child.stdout.on('data', function(data) {
-    stdout += data.toString();
-  });
-  child.stderr.on('data', function(data) {
-    stderr += data.toString();
-  });
-  child.on( 'close', function(code) {
-    console.log("child closed");
-    child.stdin.end();
-    cb((code===0 ? null : {code: code}),stdout,stderr);
-  });
-}
-
-function unquote(s) {
-  return s.replace(/^(?:\"(.*)\"|'(.*)')$/, "$1$2");
-}
-*/
